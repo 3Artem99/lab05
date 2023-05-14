f5295f80f948a3a7f41060d9f3f7e190b4ca0e8d
3Artem99 <113392103+3Artem99@users.noreply.github.com>
2023-05-14 16:46:22 +0300

    Update test1.cpp

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -1,15 +1,35 @@
-#include <Account.h>
-#include <gtest/gtest.h>
+#include <Account.h> 
+#include <gtest/gtest.h> 
+#include <gmock/gmock.h> 
+
+using namespace testing;
+
+class MockAccount : public Account {
+ public:
+  MOCK_METHOD0(GetBalance, int());
+  MOCK_METHOD1(ChangeBalance, void(int));
+  MOCK_METHOD0(Lock, void());
+  MOCK_METHOD0(Unlock, void());
+};
 
 TEST(Account, Banking){
-	Account test(0,0);
-	
-	ASSERT_EQ(test.GetBalance(), 0);
-	ASSERT_THROW(test.ChangeBalance(100), std::runtime_error);
-	test.Lock();
-	ASSERT_NO_THROW(test.ChangeBalance(100));
-	ASSERT_EQ(test.GetBalance(), 100);
-	ASSERT_THROW(test.Lock(), std::runtime_error);
-	test.Unlock();
-	ASSERT_THROW(test.ChangeBalance(100), std::runtime_error);
+  MockAccount test;
+  EXPECT_CALL(test, GetBalance())
+      .WillOnce(Return(0));
+  EXPECT_CALL(test, ChangeBalance(100))
+      .WillOnce(Throw(std::runtime_error("Account is locked")));
+  EXPECT_CALL(test, Lock());
+  EXPECT_CALL(test, ChangeBalance(100));
+  EXPECT_CALL(test, GetBalance())
+      .WillOnce(Return(100));
+  EXPECT_CALL(test, Lock())
+      .WillOnce(Throw(std::runtime_error("Account is already locked")));
+  EXPECT_CALL(test, Unlock());
+  EXPECT_CALL(test, ChangeBalance(100))
+      .WillOnce(Throw(std::runtime_error("Account is locked")));
+
+  test.Lock();
+  ASSERT_THROW(test.ChangeBalance(100), std::runtime_error);
+  test.Unlock();
+  ASSERT_THROW(test.ChangeBalance(100), std::runtime_error);
 }
