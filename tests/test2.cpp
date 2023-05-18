f3c64b5cd6de5329e31592003744cbb0f85b312e
3Artem99 <113392103+3Artem99@users.noreply.github.com>
2023-05-18 17:38:46 +0300

    Update test2.cpp

--- tests/test2.cpp
+++ tests/test2.cpp
@@ -1,34 +1,69 @@
 #include <Account.h>
 #include <Transaction.h>
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
-TEST(Transaction, Banking){
-//создаём константы. base_fee для полноценного теста должен быть хотя бы 51.
-	const int base_A = 5000, base_B = 5000, base_fee = 100;
-//создаём тестовые объекты
-	Account Alice(0,base_A), Bob(1,base_B);
-	Transaction test_tran;
-//проверяем конструктор по умолчанию и сеттеры-геттеры
-	ASSERT_EQ(test_tran.fee(), 1);
-	test_tran.set_fee(base_fee);
-	ASSERT_EQ(test_tran.fee(), base_fee);
-	
-//проверяем случаи когда транзакция не проходит
-	ASSERT_THROW(test_tran.Make(Alice, Alice, 1000), std::logic_error);
-	ASSERT_THROW(test_tran.Make(Alice, Bob, -50), std::invalid_argument);
-	ASSERT_THROW(test_tran.Make(Alice, Bob, 50), std::logic_error);
-	if (test_tran.fee()*2-1 >= 100)
-		ASSERT_EQ(test_tran.Make(Alice, Bob, test_tran.fee()*2-1), false);
-//проверяем, что всё правильно лочится
-	Alice.Lock();
-	ASSERT_THROW(test_tran.Make(Alice, Bob, 1000), std::runtime_error);
-	Alice.Unlock();
-//проверяем что если входные параметры правильные, то транзакция проходит правильно
-	ASSERT_EQ(test_tran.Make(Alice, Bob, 1000), true);
-	ASSERT_EQ(Bob.GetBalance(), base_B+1000);	
-	ASSERT_EQ(Alice.GetBalance(), base_A-1000-base_fee);
-//проверяем что транзакция не проходит, если не хватает средств
-	ASSERT_EQ(test_tran.Make(Alice, Bob, 3900), false);
-	ASSERT_EQ(Bob.GetBalance(), base_B+1000);	
-	ASSERT_EQ(Alice.GetBalance(), base_A-1000-base_fee);
+// создаем mock-объект класса Account
+class MockAccount : public Account {
+public:
+  MOCK_METHOD(void, Lock, ());
+  MOCK_METHOD(void, Unlock, ());
+  MOCK_METHOD(bool, ChangeBalance, (int));
+  MOCK_METHOD(int, GetBalance, (), (const));
+};
+
+// создаем тестовый класс
+class TransactionTest : public ::testing::Test {
+protected:
+  // создаем mock-объекты и тестируемый объект
+  MockAccount Alice, Bob;
+  Transaction test_tran;
+
+  // инициализируем mock-объекты
+  void SetUp() override {
+    Alice.SetBalance(5000);
+    Bob.SetBalance(5000);
+  }
+};
+
+// тестирование конструктора по умолчанию и сеттеров-геттеров
+TEST_F(TransactionTest, DefaultConstructorAndSettersAndGetters) {
+  EXPECT_EQ(test_tran.fee(), 1);
+  test_tran.set_fee(100);
+  EXPECT_EQ(test_tran.fee(), 100);
+}
+
+// тестирование случаев, когда транзакция не проходит
+TEST_F(TransactionTest, TransactionFails) {
+  EXPECT_THROW(test_tran.Make(Alice, Alice, 1000), std::logic_error);
+  EXPECT_THROW(test_tran.Make(Alice, Bob, -50), std::invalid_argument);
+  EXPECT_THROW(test_tran.Make(Alice, Bob, 50), std::logic_error);
+  if (test_tran.fee()*2-1 >= 100)
+    EXPECT_FALSE(test_tran.Make(Alice, Bob, test_tran.fee()*2-1));
+}
+
+// тестирование лоча и анлоча
+TEST_F(TransactionTest, LockAndUnlock) {
+  EXPECT_CALL(Alice, Lock()).Times(1);
+  EXPECT_THROW(test_tran.Make(Alice, Bob, 1000), std::runtime_error);
+  EXPECT_CALL(Alice, Unlock()).Times(1);
+}
+
+// тестирование успешной транзакции
+TEST_F(TransactionTest, SuccessfulTransaction) {
+  EXPECT_CALL(Alice, Lock()).Times(1);
+  EXPECT_CALL(Bob, ChangeBalance(1000)).WillOnce(::testing::Return(true));
+  EXPECT_CALL(Alice, ChangeBalance(-1100)).WillOnce(::testing::Return(true));
+  EXPECT_CALL(Alice, Unlock()).Times(1);
+  EXPECT_TRUE(test_tran.Make(Alice, Bob, 1000));
+  EXPECT_EQ(Bob.GetBalance(), 6000);
+  EXPECT_EQ(Alice.GetBalance(), 3900);
+}
+
+// тестирование неуспешной транзакции из-за нехватки средств
+TEST_F(TransactionTest, FailedTransactionNotEnoughMoney) {
+  EXPECT_CALL(Alice, Lock()).Times(1);
+  EXPECT_FALSE(test_tran.Make(Alice, Bob, 3900));
+  EXPECT_EQ(Bob.GetBalance(), 5000);
+  EXPECT_EQ(Alice.GetBalance(), 5000);
 }
