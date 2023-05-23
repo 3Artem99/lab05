89256737590a807faed58080b90f65c5c70964f5
3Artem99 <113392103+3Artem99@users.noreply.github.com>
2023-05-23 15:52:51 +0300

    Update test1.cpp

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -1,37 +1,28 @@
 #include <Account.h>
 #include <gtest/gtest.h>
-
+#include <gmock/gmock.h>
 
 class MockAccount : public Account {
 public:
-MOCK_METHOD(int, GetBalance, ());
-MOCK_METHOD(void, ChangeBalance, (int));
-MOCK_METHOD(void, Lock, ());
-MOCK_METHOD(void, Unlock, ());
+
+  using Account::Account;
+  MOCK_METHOD(int,GetBalance, (), (const));
+  MOCK_METHOD(void, ChangeBalance, (int diff));
+  MOCK_METHOD(void,Lock, ());
+  MOCK_METHOD(void,Unlock, ());
 };
 
-MockAccount mock_account(0,0);
+MockAccount account(0,0);
 
-TEST(Account, Banking){
-// Настраиваем Mock объект
-EXPECT_CALL(mock_account, GetBalance())
-.WillOnce(testing::Return(0));
-EXPECT_CALL(mock_account, ChangeBalance(100))
-.WillOnce(testing::Throw(std::runtime_error("Account is locked")));
-EXPECT_CALL(mock_account, Lock());
-EXPECT_CALL(mock_account, Unlock());
-EXPECT_CALL(mock_account, ChangeBalance(100))
-.WillOnce(testing::Return());
-
-// Используем Mock объект вместо реального объекта
-Account* account = &mock_account;
-
-ASSERT_EQ(account->GetBalance(), 0);
-ASSERT_THROW(account->ChangeBalance(100), std::runtime_error);
-account->Lock();
-ASSERT_NO_THROW(account->ChangeBalance(100));
-ASSERT_EQ(account->GetBalance(), 100);
-ASSERT_THROW(account->Lock(), std::runtime_error);
-account->Unlock();
-ASSERT_THROW(account->ChangeBalance(100), std::runtime_error);
+TEST(Account, Banking) {
+  EXPECT_CALL(account, GetBalance())
+    .WillOnce(testing::Return(0));
+    EXPECT_CALL(account, Lock());
+  EXPECT_CALL(account, ChangeBalance(100))
+    .WillOnce(testing::Throw(std::runtime_error("Account is locked")));
+  
+  EXPECT_CALL(account, Unlock());
+  EXPECT_CALL(account, ChangeBalance(100))
+    .WillOnce(testing::Return());
+  
 }
